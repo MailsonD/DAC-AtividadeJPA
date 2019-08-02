d2201489362c337e9bd938da009d2dfa73dc7714
MailsonD <mailssondennis@gmail.com>
2019-08-02 16:07:16 -0300

    Questões C e D em Criteria para modelo 2

--- src/main/java/com/ifpb/atividadConsultas/principal/PrincipalBean.java
+++ src/main/java/com/ifpb/atividadConsultas/principal/PrincipalBean.java
@@ -32,19 +32,25 @@ public class PrincipalBean {
         cb = em.getCriteriaBuilder();
 //        questao_A_JPQL();
 //        questao_A_CRITERIA();
-        questao_B_JPQL();
+//        questao_B_JPQL();
 //        questao_B_CRITERIA();
 //        questao_C_JPQL();
-//        questao_C_CRITERIA();
+        questao_C_CRITERIA();
 //        questao_D_JPQL();
 //        questao_D_CRITERIA();
     }
 
 
-
     private void questao_A_JPQL() {
-        String jpql = "SELECT ";
-        em.createQuery(jpql,Object[].class);
+        String jpql = "SELECT p FROM Pessoa p WHERE     ";
+        TypedQuery<Object[]> query = em.createQuery(jpql, Object[].class);
+        query.getResultList().forEach(p -> {
+            System.out.println(p[0]);
+            System.out.println(p[1]);
+            System.out.println(p[2]);
+            System.out.println(p[3]);
+        });
+
     }
 
     private void questao_B_JPQL() {
@@ -58,11 +64,39 @@ public class PrincipalBean {
         System.out.println("Foi");
     }
 
+    private void questao_B_CRITERIA() {
+        CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
+        Root<Revisor> fromRevisor = query.from(Revisor.class);
+        Root<Area> fromArea = query.from(Area.class);
+        Join<Object, Object> joinPub = fromRevisor.join("publicacoes");
+        Join<Object, Object> joinAreas = joinPub.join("areas");
+
+//        cb.isMember(,joinAreas);
+
+
+    }
+
     private void questao_C_JPQL() {
         String jpql = "SELECT r.nome FROM Revisor r, IN(r.publicacoes) p WHERE p.titulo LIKE :expressao";
         TypedQuery<String> query = em.createQuery(jpql, String.class);
         query.setParameter("expressao","Java%");
         query.getResultList().forEach(System.out::println);
+    }
+
+    private void questao_C_CRITERIA() {
+        CriteriaQuery<String> query = cb.createQuery(String.class);
+        Root<Revisor> from = query.from(Revisor.class);
+
+        Join<Object, Object> join = from.join("publicacoes");
+
+        Predicate like = cb.like(join.get("titulo"), "Java%");
+
+        query.select(from.get("nome")).where(like);
+
+        em.createQuery(query)
+                .getResultList()
+                .forEach(System.out::println);
+
     }
 
     private void questao_D_JPQL() {
@@ -73,6 +107,26 @@ public class PrincipalBean {
             System.out.println("Nome: "+a[0]+" | Quantidade: "+a[1]);
         });
     }
+
+    private void questao_D_CRITERIA() {
+        CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
+        Root<Escritor> from = query.from(Escritor.class);
+
+        Join<Object, Object> join = from.join("publicacoes");
+
+        Predicate equal = cb.equal(from.get("premios"), 3);
+
+        query.multiselect(from.get("nome"),cb.count(join))
+                .where(equal)
+                .groupBy(from.get("nome"));
+
+         em.createQuery(query)
+                 .getResultList()
+                 .forEach(v -> {
+                     System.out.println("Nome: "+v[0]+" | Quantidade: "+v[1]);
+                 });
+    }
+
 
 
 
